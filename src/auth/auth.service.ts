128ae388d82431de38e1ca7bf31b068c6689d2de
Tan Vo <tanvo@snappymob.com>
2022-06-14 09:45:44 +0700

    Fix issue RepositoryNotFoundError

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials-dto';
-import { UsersRepository } from './users.repository';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(UsersRepository)
-    private usersRepository: UsersRepository,
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    this.usersRepository.create(authCredentialsDto);
+    const { username, password } = authCredentialsDto;
+    const user = this.usersRepository.create({ username, password });
+
+    await this.usersRepository.save(user);
   }
 }
